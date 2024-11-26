875d6433d4bf8a7895a62662c6b035404afb1c7c
gooddfree <gooddvoix@gmail.com>
2024-11-26 14:49:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-
+from kivy.uix.button import Button
 
 class TriangleApp(App):
     def build(self):
